4d5068b64c04b0bd207d03df98ab9208e6453e2b
Pushpak <mpushpakteja@gmail.com>
2017-09-10 23:46:06 +0530

    Fixed the issues from the review

--- scripts/app.js
+++ scripts/app.js
@@ -62,100 +62,97 @@ function initMap() {
         zoom: 13
     });
 
-    //ViewModel
-    var ViewModel = function() {
-        'use strict';
-        var self = this;
-        this.restaurantList = ko.observableArray([]);
-        this.query = ko.observable('');
-        this.getContentString = function(restaurant) {
-            return "<img src='{0}' alt='{1}'><br/><h4>{1}</h4><br/><p>{2}</p><p><strong>Cuisines:</strong> {3}</p><p><strong>Rating:</strong> <span style='background: #{7};color:white;border-radius: 4px;font-size: 16px; height: 25px;line-height: 23px;font-weight:bold;text-align: center;width: 36px;margin: 0 auto;'>{8}</span></p><a href='{4}'>Menu</a>&nbsp;&nbsp;&nbsp;&nbsp;<a href='{6}'>Photos</a>&nbsp;&nbsp;&nbsp;&nbsp;<a href='{5}'>More...</a>"
-                .format(restaurant.thumb, restaurant.name, restaurant.location.address, restaurant.cuisines, restaurant.menu_url, restaurant.url, restaurant.photos_url, restaurant.user_rating.rating_color, restaurant.user_rating.aggregate_rating);
-        };
-        this.getRestaurants = ko.computed(function() {
-
-            var params = {
-                lat: initLat,
-                lon: initLong,
-                radius: 5000,
-                collection_id: 51, //Brilliant biryani's collection from Zomato (found using their collections API)
-                sort: 'rating',
-                order: 'desc'
-            };
-            $.ajax({
-                url: 'https://developers.zomato.com/api/v2.1/search',
-                headers: {
-                    'Accept': 'application/json',
-                    'user-key': '7e6baf0a4c94c6ab35ee9c936d72f35b'
-                },
-                method: 'GET',
-                dataType: 'json',
-                data: params,
-                async: true,
-            }).done(function(response) {
-                allRestaurants = response.restaurants;
-                allRestaurants.forEach(function(restaurant) {
-                    var marker = new google.maps.Marker({
-                        position: { lat: parseFloat(restaurant.restaurant.location.latitude), lng: parseFloat(restaurant.restaurant.location.longitude) },
-                        map: map,
-                        title: restaurant.restaurant.name
-                    });
-                    var contentString = self.getContentString(restaurant.restaurant);
-                    var infowindow = new google.maps.InfoWindow({
-                        content: contentString
-                    });
-                    marker.addListener('click', function() {
-                        infowindow.open(map, marker); //open the selected marker's infoWindow
-                        toggleBounce(marker);
-                    });
-                    google.maps.event.addListener(infowindow, 'closeclick', function() {
-                        marker.setAnimation(null);
-                    });
-                    //Close infoWindow by clicking anywhere on the map
-                    map.addListener("click", function() {
-                        infowindow.close(infowindow);
-                        marker.setAnimation(null);
-                    });
-                    markers.push(marker);
-                    infoWindows.push(infowindow);
-                    restaurant.restaurant.marker = marker;
-                    restaurant.restaurant.infoWindow = infowindow;
-                    self.restaurantList.push(new Restaurant(restaurant.restaurant));
-                });
-            }).fail(function(response) {
-                alert('Could not load restaurant details from the Zomato server');
-            });
-        }, this);
-
-        this.filteredRestaurants = function() {
-            var filteredRestaurants = [];
-            self.restaurantList([]); //resetting restaurantList
-            resetMarkers();
-
-            var filterString = self.query().toLowerCase();
-
-            //moving filtered restaurants to filteredRestaurants and hiding all the markers
-            allRestaurants.forEach(function(restaurant) {
-                if (restaurant.restaurant.name.toLowerCase().indexOf(filterString) != -1)
-                    filteredRestaurants.push(restaurant);
-                restaurant.restaurant.marker.setVisible(false);
-            });
-
-            //Showing filtered restaurants
-            filteredRestaurants.forEach(function(restaurant) {
-                restaurant.restaurant.marker.setVisible(true);
-                self.restaurantList.push(new Restaurant(restaurant.restaurant));
-            });
-        };
-    };
-
     //Apply knockout bindings
     ko.applyBindings(new ViewModel());
 }
 
+//ViewModel
+var ViewModel = function() {
+    'use strict';
+    var self = this;
+    this.restaurantList = ko.observableArray([]);
+    this.query = ko.observable('');
+    this.getContentString = function(restaurant) {
+        return "<img src='{0}' alt='{1}'><br/><h4>{1}</h4><br/><p>{2}</p><p><strong>Cuisines:</strong> {3}</p><p><strong>Rating:</strong> <span style='background: #{7};color:white;border-radius: 4px;font-size: 16px; height: 25px;line-height: 23px;font-weight:bold;text-align: center;width: 36px;margin: 0 auto;'>{8}</span></p><a href='{4}'>Menu</a>&nbsp;&nbsp;&nbsp;&nbsp;<a href='{6}'>Photos</a>&nbsp;&nbsp;&nbsp;&nbsp;<a href='{5}'>More...</a>"
+            .format(restaurant.thumb, restaurant.name, restaurant.location.address, restaurant.cuisines, restaurant.menu_url, restaurant.url, restaurant.photos_url, restaurant.user_rating.rating_color, restaurant.user_rating.aggregate_rating);
+    };
+    this.getRestaurants = ko.computed(function() {
+
+        var params = {
+            lat: initLat,
+            lon: initLong,
+            radius: 5000,
+            collection_id: 51, //Brilliant biryani's collection from Zomato (found using their collections API)
+            sort: 'rating',
+            order: 'desc'
+        };
+        $.ajax({
+            url: 'https://developers.zomato.com/api/v2.1/search',
+            headers: {
+                'Accept': 'application/json',
+                'user-key': '7e6baf0a4c94c6ab35ee9c936d72f35b'
+            },
+            method: 'GET',
+            dataType: 'json',
+            data: params,
+            async: true,
+        }).done(function(response) {
+            allRestaurants = response.restaurants;
+            allRestaurants.forEach(function(restaurant) {
+                var marker = new google.maps.Marker({
+                    position: { lat: parseFloat(restaurant.restaurant.location.latitude), lng: parseFloat(restaurant.restaurant.location.longitude) },
+                    map: map,
+                    title: restaurant.restaurant.name
+                });
+                var contentString = self.getContentString(restaurant.restaurant);
+                var infowindow = new google.maps.InfoWindow({
+                    content: contentString
+                });
+                marker.addListener('click', function() {
+                    resetMarkers();
+                    infowindow.open(map, marker); //open the selected marker's infoWindow
+                    toggleBounce(marker);
+                });
+                google.maps.event.addListener(infowindow, 'closeclick', function() {
+                    marker.setAnimation(null);
+                });
+                //Close infoWindow by clicking anywhere on the map
+                map.addListener("click", function() {
+                    resetMarkers();
+                });
+                markers.push(marker);
+                infoWindows.push(infowindow);
+                restaurant.restaurant.marker = marker;
+                restaurant.restaurant.infoWindow = infowindow;
+                self.restaurantList.push(new Restaurant(restaurant.restaurant));
+            });
+        }).fail(function(response) {
+            alert('Could not load restaurant details from the Zomato server');
+        });
+    }, this);
+
+    this.filteredRestaurants = function() {
+        var filteredRestaurants = [];
+        self.restaurantList([]); //resetting restaurantList
+        resetMarkers();
+
+        var filterString = self.query().toLowerCase();
+
+        //moving filtered restaurants to filteredRestaurants and hiding all the markers
+        allRestaurants.forEach(function(restaurant) {
+            if (restaurant.restaurant.name.toLowerCase().indexOf(filterString) != -1)
+                filteredRestaurants.push(restaurant);
+            restaurant.restaurant.marker.setVisible(false);
+        });
+
+        //Showing filtered restaurants
+        filteredRestaurants.forEach(function(restaurant) {
+            restaurant.restaurant.marker.setVisible(true);
+            self.restaurantList.push(new Restaurant(restaurant.restaurant));
+        });
+    };
+};
 //Error handling for map
 function mapLoadError() {
-    var img = document.createElement("IMG");
-    img.src = "images/GoogleMapLoadError.jpg";
-    $('#imageDiv').appendChild(img);
+    alert("Map could not load properly");
 }
